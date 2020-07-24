a38d1da575882a4c41a2365fb87df9d3be6f1071
Martin Durant <martin.durant@utoronto.ca>
2020-07-24 15:51:38 -0400

    Remove extra

--- gdrivefs/core.py
+++ gdrivefs/core.py
@@ -1,24 +1,12 @@
-import os
 import pickle
 import re
 import json
 
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.errors import HttpError
 
 from fsspec.spec import AbstractFileSystem, AbstractBufferedFile
 
-not_secret = {"client_id": "464800473488-54uc38r5jos4pmk7vqfhg58jjjtd6vr9"
-                           ".apps.googleusercontent.com",
-              "client_secret": "919dmsddLQRbbXkt3-B7gFYd"}
-client_config = {'installed': {
-    'client_id': not_secret['client_id'],
-    'client_secret': not_secret['client_secret'],
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://accounts.google.com/o/oauth2/token"
-}}
-tfile = os.path.join(os.path.expanduser("~"), '.google_drive_tokens')
 scope_dict = {'full_control': 'https://www.googleapis.com/auth/drive',
               'read_only': 'https://www.googleapis.com/auth/drive.readonly'}
 
@@ -53,15 +41,13 @@ class GoogleDriveFileSystem(AbstractFileSystem):
 
     def __init__(self, root_file_id=None, token="browser",
                  access="full_control", spaces='drive',
-                 tokens_file=None, **kwargs):
+                 **kwargs):
         super().__init__(**kwargs)
         self.access = access
         self.scopes = [scope_dict[access]]
         self.token = token
-        self.tfile = tokens_file or tfile
         self.spaces = spaces
         self.root_file_id = root_file_id or 'root'
-        self.load_tokens()
         self.connect(method=token)
         self.ls("")
 
@@ -73,25 +59,9 @@ class GoogleDriveFileSystem(AbstractFileSystem):
         else:
             raise ValueError(f"Invalid connection method `{method}`.")
 
-    def load_tokens(self):
-        """Get "browser" tokens from disc"""
-        try:
-            with open(self.tfile, 'rb') as f:
-                tokens = pickle.load(f)
-        except Exception as e:
-            tokens = {}
-        GoogleDriveFileSystem.tokens = tokens
-
-    def _save_tokens(self):
-        with open(self.tfile, 'wb') as f:
-            pickle.dump(GoogleDriveFileSystem.tokens, f, 2)
-
     def _connect_browser(self):
         import pydata_google_auth
         credentials = pydata_google_auth.get_user_credentials(self.scopes)
-
-        self.tokens[self.access] = credentials
-        self._save_tokens()
         srv = build('drive', 'v3', credentials=credentials)
         self._drives = srv.drives()
         self.service = srv.files()
